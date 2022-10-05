3d31f50fbda44b722cf17221e535419623ead509
Vadim Liventsev <hello@vadim.me>
2022-10-05 17:07:51 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="programlib",
-    version="2.0.1",
+    version="2.1.0",
     description="Programs as Objects",
     long_description=README,
     long_description_content_type="text/markdown",
