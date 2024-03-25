4d24726f73c5cd50e61134d9162f1551c52fa02a
Vadim Liventsev <dev@vadim.me>
2024-03-25 12:53:43 +0100

    Error handling

--- programlib/pexpectutil.py
+++ programlib/pexpectutil.py
@@ -6,7 +6,8 @@ def pexpect_exceptions(f):
             return f(*args, **kwargs)
         except pexpect.exceptions.TIMEOUT as e:
             raise RuntimeError('Program did not receive expected input') from e
-        except pexpect.exceptions.EOF as e:
+        except (pexpect.exceptions.EOF, OSError) as e:
+            # I/O related OSError is almost always due to the program exiting
             raise RuntimeError('Program exited unexpectedly') from e
         except pexpect.exceptions.ExceptionPexpect as e:
             raise RuntimeError('Program failed') from e
